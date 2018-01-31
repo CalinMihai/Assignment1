ce657475a99a9f70895be2364bba8a4e363278c6
Calin Mihai <32879010+CalinMihai@users.noreply.github.com>
2018-01-31 17:00:55 +0200

    Update 7.c

--- 7.c
+++ 7.c
@@ -4,10 +4,8 @@ void main()
     int i = 412;
     int j = 100;
     double x = 987.654321;
-        //(a)
     double a = i / j;
     double b = (double) (i / j);
     printf("\n a = %d", a);
     printf("\n b = %d", b);
-    //(b)
 }
